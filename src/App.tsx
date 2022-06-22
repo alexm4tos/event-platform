b3a0beb637de8bfb223fc8b02b3fd6411220d57a
Alex Matos <alex.matos.freitas@gmail.com>
2022-06-21 21:10:21 -0300

    add event page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Event } from './pages/Event';
+
 function App() {
 	return (
-		<div className='h-screen flex items-center justify-center'>
-			<h1 className='text-4xl'>Vite App</h1>
+		<div>
+			<Event />
 		</div>
 	);
 }
