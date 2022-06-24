5261cd0b2c277a54dad5549b836039005db1b326
Alex Matos <alex.matos.freitas@gmail.com>
2022-06-24 13:48:24 -0300

    add auth token header on apollo service

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,5 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
 	uri: import.meta.env.VITE_GRAPHQL_API_URL,
+	headers: {
+		Authorization: `Bearer ${import.meta.env.VITE_GRAPHQL_AUTH_TOKEN}`,
+	},
 	cache: new InMemoryCache(),
 });
